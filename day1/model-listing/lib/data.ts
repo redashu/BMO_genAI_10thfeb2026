05c2591341dd948e6ad8dfe0bf3d85b137ecf2a8
Ashutoshh <ashutoshh@linux.com>
2026-02-11 01:25:27 +0530

    updates

---
+++ day1/model-listing/lib/data.ts
@@ -0,0 +1,51 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+
+
+export class BedrockModelListerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here lambda funcation here 
+    const modelListerLambda = new lambda.Function(this, 'ModelListerFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      timeout: cdk.Duration.seconds(30),
+    });
+
+     // IAM permission for Control Plane
+    modelListerLambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'bedrock:ListFoundationModels'
+        ],
+        resources: ['*'],
+      })
+    );
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'BedrockModelApi', {
+      restApiName: 'Bedrock Model Listing API',
+      deployOptions: {
+        stageName: 'prod',
+      },
+    });
+
+    const modelsResource = api.root.addResource('models');
+
+    modelsResource.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(modelListerLambda)
+    );
+
+    // Output API URL
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: `${api.url}models`,
+    });
+}
+}
