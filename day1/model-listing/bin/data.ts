a534e8d49753689eebda2eec518ee9061ddea520
Ashutoshh <ashutoshh@linux.com>
2026-02-10 23:12:31 +0530

    updates

---
+++ day1/model-listing/bin/data.ts
@@ -0,0 +1,22 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+
+
+export class BedrockModelListerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here lambda funcation here 
+    const modelListerLambda = new lambda.Function(this, 'ModelListerFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      timeout: cdk.Duration.seconds(30),
+    });
+
+}
+}
